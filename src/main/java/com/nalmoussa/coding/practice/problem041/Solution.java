ef8dd11bccc526503e2604b419e081f757b167df
nalmoussa <nalmoussa@gmail.com>
2020-10-06 10:10:39 -0700

    Starting problem 41

---
+++ src/main/java/com/nalmoussa/coding/practice/problem041/Solution.java
@@ -0,0 +1,7 @@
+package com.nalmoussa.coding.practice.problem041;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello World!! Welcome " + args[0]);
+    }
+}
